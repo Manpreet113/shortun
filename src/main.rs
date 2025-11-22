86753d732232676971b53fab82fda0876bfc9380
Manpreet <97226905+Manpreet113@users.noreply.github.com>
2025-11-22 21:26:52 +0530

    feat: add URL validation and sanitation in slug creation

--- src/main.rs
+++ src/main.rs
@@ -10,6 +10,7 @@ use axum::{
 use serde::{Deserialize, Serialize};
 mod db;
 use db::PostgresStorage;
+use url::Url;
 
 #[derive(Deserialize)]
 struct CreateRequest{
@@ -22,12 +23,18 @@ struct CreateResponse{
 }
 
 async fn create_slug(State(storage): State<PostgresStorage>, Json(payload): Json<CreateRequest>) -> impl IntoResponse{
-    let id = storage.shorten(&payload.url).await;
+
+    let valid_url = match validate_and_sanitize_url(&payload.url) {
+        Ok(url) => url,
+        Err(e) => return (StatusCode::BAD_REQUEST, e).into_response(), 
+    };
+
+    let id = storage.shorten(&valid_url).await;
 
     let slug = format!("http://localhost:3000/{}", id);
 
     let response = CreateResponse {slug};
-    (StatusCode::CREATED, Json(response))
+    (StatusCode::CREATED, Json(response)).into_response()
 }
 
 async fn redirect(State(storage): State<PostgresStorage>, Path(id): Path<String>) -> impl IntoResponse{
@@ -35,6 +42,22 @@ async fn redirect(State(storage): State<PostgresStorage>, Path(id): Path<String>
         Some(url) => Redirect::permanent(&url).into_response(),
         None => (StatusCode::NOT_FOUND, "URL Not Found!" ).into_response(), 
     }
+}
+
+fn validate_and_sanitize_url(raw_url: &str) -> Result<String, String> {
+    let url_to_parse = if raw_url.starts_with("http://") || raw_url.starts_with("https://") {
+        raw_url.to_string()
+} else {
+    format!("https://{}", raw_url)
+    };
+
+    let parsed = Url::parse(&url_to_parse).map_err(|_| "Invalid URL format".to_string())?;
+
+    if parsed.scheme() != "http" && parsed.scheme() != "https" {
+        return Err("Only HTTP and HTTPS schemes are allowed".to_string());
+    }
+
+    Ok(parsed.to_string())
 }
 
 #[tokio::main]
