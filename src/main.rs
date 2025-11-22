a44b8f8251b92c6be810a3e231ac73dc4d09969e
Manpreet <97226905+Manpreet113@users.noreply.github.com>
2025-11-22 16:14:29 +0530

    Refactor main and storage modules to implement URL shortening and retrieval with async methods

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,47 @@
-fn main() {
-    println!("Hello, world!");
+mod storage;
+use storage::{MemoryStorage, Storage};
+use axum::{
+    extract::{State, Path},
+    routing::{get, post},
+    Json, Router, response::{IntoResponse, Redirect},
+    http::StatusCode,
+};
+use serde::{Deserialize, Serialize};
+
+#[derive(Deserialize)]
+struct CreateRequest{
+    url: String,
+}
+
+#[derive(Serialize)]
+struct CreateResponse{
+    slug: String,
+}
+
+async fn create_slug(State(storage): State<MemoryStorage>, Json(payload): Json<CreateRequest>) -> impl IntoResponse{
+    let id = storage.shorten(&payload.url).await;
+
+    let slug = format!("http://localhost:3000/{}", id);
+
+    let response = CreateResponse {slug};
+    (StatusCode::CREATED, Json(response))
+}
+
+async fn redirect(State(storage): State<MemoryStorage>, Path(id): Path<String>) -> impl IntoResponse{
+    match storage.get_url(&id).await {
+        Some(url) => Redirect::permanent(&url).into_response(),
+        None => (StatusCode::NOT_FOUND, "URL Not Found!" ).into_response(), 
+    }
+}
+
+#[tokio::main]
+async fn main(){
+    let storage = MemoryStorage::new();
+
+    let app = Router::new().route("/", post(create_slug)).route("/:id", get(redirect)).with_state(storage);
+
+    let listner = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
+
+    print!("Server chalu at http://0.0.0.0:3000");
+    axum::serve(listner, app).await.unwrap();
 }
--- src/storage.rs
+++ src/storage.rs
@@ -1,26 +1,36 @@
-use std::collections::Hashmap;
+use std::collections::HashMap;
 use std::sync::Arc;
 use std::sync::atomic::{AtomicU64, Ordering};
 use tokio::sync::RwLock;
+use async_trait::async_trait;
+
+#[async_trait]
+pub trait Storage: Send + Sync + 'static {
+    // Takes a long URL, returns the ID (e.g., "100")
+    async fn shorten(&self, url: &str) -> String;
+
+    // Takes an ID, returns the long URL if it exists
+    async fn get_url(&self, id: &str) -> Option<String>;
+}
 
 #[derive(Clone)]
 pub struct MemoryStorage {
     counter: Arc<AtomicU64>,
-    data: Arc<RwLock<Hashmap<String, String>>>,
+    data: Arc<RwLock<HashMap<String, String>>>,
 }
 
 impl MemoryStorage {
     pub fn new() -> Self {
         Self {
             counter: Arc::new(AtomicU64::new(0)),
-            data: Arc::new(RwLock::new(Hashmap::new())),
+            data: Arc::new(RwLock::new(HashMap::new())),
         }
     }
 }
 // Derive macro because standard Rust traits struggle
 // with returning Futures (async) dynamically.
 #[async_trait]
-impl Stroage for MemoryStorage {
+impl Storage for MemoryStorage {
     async fn shorten(&self, url: &str) -> String {
         let id = self.counter.fetch_add(1, Ordering::Relaxed); // Add 1 to counter to get id and Ordering relaxed cause who needs strictness for id gen
 
@@ -30,7 +40,7 @@ impl Stroage for MemoryStorage {
 
         let mut map = self.data.write().await;
 
-        map.insert(id_string.clone(), url.to_string);
+        map.insert(id_string.clone(), url.to_string());
 
         id_string
     }
